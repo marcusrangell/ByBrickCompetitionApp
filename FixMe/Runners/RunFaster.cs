542d9515e196ef53f03e5829fec4a998d40f6bed
marcusrangell <marcus.rangell@gmail.com>
2022-12-28 00:27:49 +0100

    Move StreamWriter declaration out from loop.

--- FixMe/Runners/RunFaster.cs
+++ FixMe/Runners/RunFaster.cs
@@ -20,37 +20,35 @@ namespace FixMe.Runners
             options = new RunnerOptions();
             _all_the_cheats = Fill.FillDataObjectArray();
         }
-        
+
         [Benchmark]
-        public void Run() 
-        {               
-            
-            for (int i = 0; i<_all_the_cheats.Count(); i++)
-            {                    
-                var cheatType = _all_the_cheats[i];
-                if (cheatType is Cuirckle)
+        public void Run()
+        {
+            using (StreamWriter swc = File.AppendText(options.CuirckleFilePath))
+            using (StreamWriter swt = File.AppendText(options.AtrangleFilePath))
+
+                for (int i = 0; i < _all_the_cheats.Count(); i++)
                 {
-                        
-                    using (StreamWriter sw = File.AppendText(options.CuirckleFilePath))
+                    var cheatType = _all_the_cheats[i];
+                    if (cheatType is Cuirckle)
                     {
-                        var _irckle = cheatType as Cuirckle;
-                        double circumference = _irckle.Calculate;
-                        sw.WriteLine($"Circumference of circle: {_irckle.RandomRadius}:{circumference}");
-                        sw.Flush();
+                        {
+                            var _irckle = cheatType as Cuirckle;
+                            double circumference = _irckle.Calculate;
+                            swc.WriteLine($"Circumference of circle: {_irckle.RandomRadius}:{circumference}");
+                            swc.Flush();
+                        }
                     }
-                        
-                }
-                else
-                {
-                    using (StreamWriter sw = File.AppendText(options.AtrangleFilePath))
+                    else
                     {
-                        var _angle = cheatType as Atrangle;
-                        double area = _angle.Calculate;
-                        sw.WriteLine($"Area of triangle: {area}");
-                        sw.Flush();
+                        {
+                            var _angle = cheatType as Atrangle;
+                            double area = _angle.Calculate;
+                            swt.WriteLine($"Area of triangle: {area}");
+                            swt.Flush();
+                        }
                     }
                 }
-            }                                   
         }
     }
 }
